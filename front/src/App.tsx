6c4d73a4a7377b28f4c750dca2e01fdb0e44c86e
Caro-L-dev <liardcar@gmail.com>
2024-05-07 11:18:02 +0200

    feat: add shadcn ui

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,14 @@
+import { Wrapper } from "./components/common/wrapper/Wrapper";
+import { Title } from "./components/common/title/Title";
+import { TypographyP } from "./components/common/typographyP/TypographyP";
+
 function App() {
-  return <div className="uppercase">Lingi Club</div>;
+  return (
+    <Wrapper className="flex flex-col items-center justify-center h-screen">
+      <Title>Lingi Club</Title>
+      <TypographyP>Under construction ... 🚧</TypographyP>
+    </Wrapper>
+  );
 }
 
 export default App;
