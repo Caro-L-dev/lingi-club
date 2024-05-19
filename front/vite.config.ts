172e8a6a0e11e0eed457990182de0d1947e1b448
macbook kal <arnaud.pantenier@gmail.com>
2024-05-19 22:17:47 +0200

    CONF Config vitest.seturp.ts + tsconfig

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -2,11 +2,26 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['./vitest.setup.ts']
     },
-  },
-});
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    }
+  }
+})
