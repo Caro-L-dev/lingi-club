b911464aadb63be593be49c69fe4b2a4bbe59f8a
Caro-L-dev <liardcar@gmail.com>
2024-05-20 14:47:59 +0200

    feat: implement static host family data

---
+++ front/src/lib/Data/hostFamilyData.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const hostFamilyData = [
+  {
+    id: 1,
+    isAvailable: true,
+    isCertified: false,
+    title: "Host Family 01",
+    photos: ["fakeImg 01", "fakeImg 02"],
+    description: "lorem ipsum",
+    price: 300,
+    region: "Ile-de-France",
+    nativeLanguage: "anglais",
+    accept: ["10-18 ans", "18-25 ans", "adulte(s)", "couple(s)"],
+    paidActivity: ["Visites et Excursions"],
+  },
+];
