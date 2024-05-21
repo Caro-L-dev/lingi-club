d643b9738caacc066a7de078dbabdecf1d176613
Anthoumani Madi <anthoumani.madi420@gmail.com>
2024-05-21 16:22:40 +0400

    feature/register

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Wrapper } from "./components/common/wrapper/Wrapper";
-import { Title } from "./components/common/title/Title";
-import { TypographyP } from "./components/common/typographyP/TypographyP";
+import Register from "./page/Register";
+import Accueil from "./page/famille-accueil/Accueil";
+import Apprenant from "./page/famille-accueil/Apprenant";
 
-function App() {
+export default function App() {
   return (
-    <Wrapper className="flex flex-col items-center justify-center h-screen">
-      <Title>Lingi Club</Title>
-      <TypographyP>Under construction ... 🚧</TypographyP>
-    </Wrapper>
+    <Router>
+      <Wrapper className="flex flex-col items-center justify-center h-screen">
+        <Routes>
+          <Route path="/accueil" element={<Accueil />} />
+          <Route path="/apprenant" element={<Apprenant />} />
+          <Route path="/" element={<Register />} />
+        </Routes>
+      </Wrapper>
+    </Router>
   );
 }
-
-export default App;
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -2,26 +2,11 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// });
-export default defineConfig(() => {
-  return {
-    plugins: [react()],
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: ['./vitest.setup.ts']
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    }
-  }
-})
+  },
+});
