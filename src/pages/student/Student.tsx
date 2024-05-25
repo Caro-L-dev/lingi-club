ea8dda1209e1a47382676493dc5a20d50bdb5202
Anthoumani Madi <anthoumani.madi420@gmail.com>
2024-05-25 19:27:11 +0400

    Remove unused dependencies and files

--- src/pages/student/Student.tsx
+++ src/pages/student/Student.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import React from "react";
 const Student = () => {
   return (
     <Card className="mx-auto max-w-sm">
