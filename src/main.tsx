f2854eb6c38d5c8fa257c234fcc399fbfa1d0076
Aurelia H <aurelia.h@hotmail.fr>
2024-05-29 17:36:46 +0200

    fix: resolve PR for firebase hooks

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 
+import { ToastContainer } from "react-toastify";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <ToastContainer
+        position="top-center"
+    />
     <App />
   </React.StrictMode>
 );
