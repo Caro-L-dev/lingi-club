5898f9e233ea5374a597df5bea02f3242e361663
Aurelia H <aurelia.h@hotmail.fr>
2024-05-30 00:58:59 +0200

    fix and feat: improvement of firebase hooks according to PR comments, creation of logOut function and addNewUser function to add user in the DB

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from "./App.tsx";
 import "./globals.css";
 
 import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ToastContainer
         position="top-center"
+        autoClose={2000}
     />
     <App />
   </React.StrictMode>
---
+++ src/types/User.ts
@@ -0,0 +1,9 @@
+export type UserType = {
+    uid: string;
+    email: string | null;
+    displayName: string | null;
+    emailVerified: boolean;
+    photoUrl: string | null;
+    creationDate: Date;
+
+}
