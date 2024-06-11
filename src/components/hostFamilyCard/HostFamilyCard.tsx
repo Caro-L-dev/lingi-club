aac942287f88e65e771282024c31b077eb32ddd2
Caro-L-dev <liardcar@gmail.com>
2024-06-11 17:25:51 +0200

    style: update interface fake families

--- src/components/hostFamilyCard/HostFamilyCard.tsx
+++ src/components/hostFamilyCard/HostFamilyCard.tsx
@@ -6,11 +6,13 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 
 interface HostFamilyCardProps {
   title: string;
+  image: string;
   description: string;
   price: number;
   region: string;
@@ -20,39 +22,51 @@ interface HostFamilyCardProps {
 
 export default function HostFamilyCard({
   title,
+  image,
   description,
   price,
   region,
   nativeLanguage,
   accept = [],
 }: HostFamilyCardProps) {
+  const defaultImage = "/public/images/family.jpg";
+
   return (
-    <Card>
+    <Card className="flex flex-col md:flex-row">
+      <CardHeader>
+        <img
+          className="h-full w-screen object-cover"
+          src={image || defaultImage}
+          alt={title}
+        />
+      </CardHeader>
+
       <CardContent>
-        <div className="flex flex-col md:flex-row md:justify-between items-center py-2">
-          <div className="flex gap-4 my-4">
-            <ItemInfo children={nativeLanguage} icon={<Flag />} />
-            <ItemInfo children={region} icon={<MapPin />} />
-            <ItemInfo children={`${price}€/sem`} icon={<Euro />} />
+        <CardContent>
+          <div className="flex flex-col md:flex-row md:justify-between items-center pt-4">
+            <div className="flex gap-4 pb-4 md:pb-0">
+              <ItemInfo children={nativeLanguage} icon={<Flag />} />
+              <ItemInfo children={region} icon={<MapPin />} />
+              <ItemInfo children={`${price}€/sem`} icon={<Euro />} />
+            </div>
+            <Button className="w-full md:w-fit">Réserver</Button>
           </div>
-          <Button className="w-full md:w-fit">Réserver</Button>
-        </div>
-      </CardContent>
+        </CardContent>
+        <CardContent className="border-t border-t-secondary pt-4">
+          <CardTitle className="text-secondary text-balance py-2">
+            Bienvenue chez {title}
+          </CardTitle>
 
-      <CardContent className="border-t border-t-secondary pt-4 mx-4">
-        <CardTitle className="text-secondary text-balance py-2">
-          Bienvenue chez {title}
-        </CardTitle>
-
-        <CardDescription className="line-clamp-3 tracking-tight my-2 mb-4">
-          {description}
-        </CardDescription>
-
-        {accept && (
-          <CardDescription className="border-t border-t-muted pt-4">
-            Accepte : {accept.join(", ")}
+          <CardDescription className="line-clamp-3 tracking-tight my-2 mb-4">
+            {description}
           </CardDescription>
-        )}
+
+          {accept.length > 0 && (
+            <CardDescription className="border-t border-t-muted pt-4">
+              Accepte : {accept.join(", ")}
+            </CardDescription>
+          )}
+        </CardContent>
       </CardContent>
     </Card>
   );
--- src/components/hostFamilyCard/HostFamilyCardList.tsx
+++ src/components/hostFamilyCard/HostFamilyCardList.tsx
@@ -1,73 +1,14 @@
+import { hostFamilies } from "@/lib/data/data";
 import HostFamilyCard from "./HostFamilyCard";
-
-const hostFamilies = [
-  {
-    id: 1,
-    title: "Maud",
-    description:
-      "Votre séjour linguistique en immersion en famille d'accueil en Angleterre Linguifamily est la 1ère plateforme française de séjours linguistiques en famille d'accueil. Créée pour faciliter l'apprentissage des langues et la découverte d'autres cultures, le site s'adresse à des jeunes, étudiants, adultes et retraités souhaitant réserver un séjour en immersion en famille d'accueil l'étranger - ou en France. Linguifamily répertorie des familles d’accueil expérimentées et soigneusement vérifiées en Angleterre, Irlande, Espagne, Italie, Malte, Etats-Unis et France Vous y trouverez des familles d'accueil proposant une immersion linguistique de qualité, avec ou sans cours de langue et activités sportives. Nous ne sommes pas un organisme de séjours linguistiques.",
-    price: 300,
-    region: "Ile-de-France",
-    nativeLanguage: "Anglais",
-    accept: ["10-18 ans", " 18-25 ans", " adulte(s)", " couple(s)"],
-  },
-  {
-    id: 2,
-    title: "les Smiths",
-    description:
-      "Votre séjour linguistique en immersion en famille d'accueil en Angleterre Linguifamily est la 1ère plateforme française de séjours linguistiques en famille d'accueil. Créée pour faciliter l'apprentissage des langues et la découverte d'autres cultures, le site s'adresse à des jeunes, étudiants, adultes et retraités souhaitant réserver un séjour en immersion en famille d'accueil l'étranger - ou en France. Linguifamily répertorie des familles d’accueil expérimentées et soigneusement vérifiées en Angleterre, Irlande, Espagne, Italie, Malte, Etats-Unis et France Vous y trouverez des familles d'accueil proposant une immersion linguistique de qualité, avec ou sans cours de langue et activités sportives. Nous ne sommes pas un organisme de séjours linguistiques.",
-    price: 350,
-    region: "Hauts-de-France",
-    nativeLanguage: "Anglais",
-    accept: ["10-18 ans"],
-  },
-  {
-    id: 3,
-    title: "Marie & Michelle",
-    description:
-      "Votre séjour linguistique en immersion en famille d'accueil en Angleterre Linguifamily est la 1ère plateforme française de séjours linguistiques en famille d'accueil. Créée pour faciliter l'apprentissage des langues et la découverte d'autres cultures, le site s'adresse à des jeunes, étudiants, adultes et retraités souhaitant réserver un séjour en immersion en famille d'accueil l'étranger - ou en France. Linguifamily répertorie des familles d’accueil expérimentées et soigneusement vérifiées en Angleterre, Irlande, Espagne, Italie, Malte, Etats-Unis et France Vous y trouverez des familles d'accueil proposant une immersion linguistique de qualité, avec ou sans cours de langue et activités sportives. Nous ne sommes pas un organisme de séjours linguistiques.",
-    price: 300,
-    region: "Auvergne",
-    nativeLanguage: "Anglais",
-  },
-  {
-    id: 4,
-    title: "Arriette",
-    description:
-      "Votre séjour linguistique en immersion en famille d'accueil en Angleterre Linguifamily est la 1ère plateforme française de séjours linguistiques en famille d'accueil. Créée pour faciliter l'apprentissage des langues et la découverte d'autres cultures, le site s'adresse à des jeunes, étudiants, adultes et retraités souhaitant réserver un séjour en immersion en famille d'accueil l'étranger - ou en France. Linguifamily répertorie des familles d’accueil expérimentées et soigneusement vérifiées en Angleterre, Irlande, Espagne, Italie, Malte, Etats-Unis et France Vous y trouverez des familles d'accueil proposant une immersion linguistique de qualité, avec ou sans cours de langue et activités sportives. Nous ne sommes pas un organisme de séjours linguistiques.",
-    price: 300,
-    region: "Ile-de-France",
-    nativeLanguage: "Anglais",
-    accept: ["10-18 ans", " 18-25 ans", " adulte(s)", " couple(s)"],
-  },
-  {
-    id: 5,
-    title: "Potter",
-    description:
-      "Votre séjour linguistique en immersion en famille d'accueil en Angleterre Linguifamily est la 1ère plateforme française de séjours linguistiques en famille d'accueil. Créée pour faciliter l'apprentissage des langues et la découverte d'autres cultures, le site s'adresse à des jeunes, étudiants, adultes et retraités souhaitant réserver un séjour en immersion en famille d'accueil l'étranger - ou en France. Linguifamily répertorie des familles d’accueil expérimentées et soigneusement vérifiées en Angleterre, Irlande, Espagne, Italie, Malte, Etats-Unis et France Vous y trouverez des familles d'accueil proposant une immersion linguistique de qualité, avec ou sans cours de langue et activités sportives. Nous ne sommes pas un organisme de séjours linguistiques.",
-    price: 350,
-    region: "Hauts-de-France",
-    nativeLanguage: "Anglais",
-    accept: ["10-18 ans"],
-  },
-  {
-    id: 6,
-    title: "Dupont",
-    description:
-      "Votre séjour linguistique en immersion en famille d'accueil en Angleterre Linguifamily est la 1ère plateforme française de séjours linguistiques en famille d'accueil. Créée pour faciliter l'apprentissage des langues et la découverte d'autres cultures, le site s'adresse à des jeunes, étudiants, adultes et retraités souhaitant réserver un séjour en immersion en famille d'accueil l'étranger - ou en France. Linguifamily répertorie des familles d’accueil expérimentées et soigneusement vérifiées en Angleterre, Irlande, Espagne, Italie, Malte, Etats-Unis et France Vous y trouverez des familles d'accueil proposant une immersion linguistique de qualité, avec ou sans cours de langue et activités sportives. Nous ne sommes pas un organisme de séjours linguistiques.",
-    price: 300,
-    region: "Auvergne",
-    nativeLanguage: "Anglais",
-  },
-];
 
 export default function HostFamilyCardList() {
   return (
-    <div className="flex flex-wrap gap-4 justify-center">
+    <div className="flex flex-wrap gap-4">
       {hostFamilies.map((hostFamily) => (
         <HostFamilyCard
           key={hostFamily.id}
           title={hostFamily.title}
+          image={hostFamily.image}
           description={hostFamily.description}
           price={hostFamily.price}
           region={hostFamily.region}
