11e4f7361cf808a90b63524b4765aad4d3b446cb
macbook kal <arnaud.pantenier@gmail.com>
2024-06-13 19:03:50 +0200

    MODIF main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,9 @@ import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
         
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <ToastContainer position="top-center" autoClose={2000} />
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <ToastContainer position="top-center" autoClose={2000} />
+        <App />
+    </React.StrictMode>
 );
